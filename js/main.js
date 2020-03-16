e86122be34c1a2d504901d0fc6537c5ef627e61e
evgenia-savenkova <savenkova-94@list.ru>
2020-03-16 22:29:34 +0300

    feat: add project section and slaider-swapper

--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector('.modal__close');
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible');
-    }
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal);
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     const modal = document.querySelector('.modal');
+//     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//     const closeBtn = document.querySelector('.modal__close');
+//     const switchModal = () => {
+//         modal.classList.toggle('modal--visible');
+//     }
+//     modalBtn.forEach(element => {
+//         element.addEventListener('click', switchModal);
 
-    });
+//     });
 
-    closeBtn.addEventListener('click', switchModal);
+//     closeBtn.addEventListener('click', switchModal);
+
+// });
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle="modal"]'),
+    closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next')
+  var prev = $('.swiper-button-prev')
+  var bullets = $('.swiper-pagination')
+
+    next.css('left', prev.width() + 10 + bullets.width() + 10)
+    bullets.css('left', prev.width() + 10)
 
 });
-
-// $(document).ready(function () {
-//     var modal = $('modal'),
-//         modalBtn = $('[data-toggle="modal"]'),
-//         closeBtn = $('.modal__close');
-
-//     modalBtn.on('click', function () {
-//         modal.toggleClass('modal--visible');
-//     });
-//     closeBtn.on('click', function () {
-//         modal.toggleClass('modal--visible');
-//     });
-// });
